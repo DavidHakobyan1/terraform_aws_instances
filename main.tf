92e7a1ea6d95460f560faacf1b1e954b208ac81f
David Hakobyan <hakobyandavid@mail.ru>
2023-01-06 20:28:48 +0400

    create instances

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_ami" "latest_ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name  = "name"
+    value = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.my_ubuntu.id
+}
+
+resource "aws_instance" "my_ubuntu" {
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name    = "My Ubuntu server"
+    Owner   = "David Hakobyan"
+    Project = "Terraform Homework"
+  }
+}
