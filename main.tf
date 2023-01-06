dd2c117bbda9fcf80a64e96f7ab6ee7c04642822
David Hakobyan <hakobyandavid@mail.ru>
2023-01-06 22:07:13 +0400

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
   region = "eu-central-1"
+  workspaces {
+    name = "gh-actions-demo"
+  }
 }
 
 data "aws_ami" "latest_ubuntu" {
