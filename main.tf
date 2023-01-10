f0e2cf9783aa6380d87c1d9f9004f37a24199fc9
David Hakobyan <hakobyandavid@mail.ru>
2023-01-10 10:16:04 +0400

    fixed

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "my_ubuntu" {
   ami           = data.aws_ami.latest_ubuntu.id
   instance_type = var.instance_type
   provisioner "local-exec" {
-    command = "echo local.cloud_providers_name Instance Creations!"
+    command = "echo ${local.cloud_providers_name} Instance Creations!"
   }
   tags = merge(var.tags, {Name = "My Ubuntu server"})
 }
